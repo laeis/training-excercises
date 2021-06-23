d5e914ac4f4adfea758088efef5c237b0746de68
Roman Kosyi <roman.kosyi@care.com>
2021-06-23 21:13:07 +0300

    updated readme

--- day_4/2/main.go
+++ day_4/2/main.go
@@ -5,8 +5,16 @@
 // service will kill your process, unless you are a paid premium user.
 //
 // Beginner Level: 10s max per request
-// Advanced Level: 10s max per user (accumulated)
+// Advanced Level: 10s max per user (accumulated) 
 // Hint: time.Ticker can be used
+// Hint 2: to calculate timediff for Advanced lvl use:
+//
+//  start := time.Now()
+//	// your work
+//	t := time.Now()
+//	elapsed := t.Sub(start) // 1s or whatever time has passed
+
+
 
 package main
 
